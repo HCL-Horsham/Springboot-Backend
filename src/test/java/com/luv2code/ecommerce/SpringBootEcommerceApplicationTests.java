900c1c60f8d803bd244ebe693c381a8ba601b951
Alex <61956478+alexlin1822@users.noreply.github.com>
2022-04-07 11:20:59 -0400

    Update SpringBootEcommerceApplicationTests.java

--- src/test/java/com/luv2code/ecommerce/SpringBootEcommerceApplicationTests.java
+++ src/test/java/com/luv2code/ecommerce/SpringBootEcommerceApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
-class SpringBootEcommerceApplicationTests {
+public class SpringBootEcommerceApplicationTests {
 
 	@Test
 	void contextLoads() {
