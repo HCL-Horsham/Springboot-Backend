0d89ffd810fd7805034817cf7928ea50015435c3
Alex <61956478+alexlin1822@users.noreply.github.com>
2022-04-07 10:50:20 -0400

    Add files via upload

---
+++ src/test/java/com/luv2code/ecommerce/AppTesting.java
@@ -0,0 +1,11 @@
+package com.luv2code.ecommerce;
+import org.junit.Test;
+
+public class AppTesting extends SpringBootEcommerceApplicationTests {
+	
+	   @Test
+	   public void main() {
+		   SpringBootEcommerceApplication.main(new String[] {});
+	   }
+	
+}
