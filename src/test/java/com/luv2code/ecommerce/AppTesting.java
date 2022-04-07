8f5e50fde67164fb5f2807eae50900199cb669e7
Alex <61956478+alexlin1822@users.noreply.github.com>
2022-04-07 11:21:32 -0400

    Update AppTesting.java

--- src/test/java/com/luv2code/ecommerce/AppTesting.java
+++ src/test/java/com/luv2code/ecommerce/AppTesting.java
@@ -1,8 +1,6 @@
 package com.luv2code.ecommerce;
 import org.junit.jupiter.api.Test;
 
-import com.luv2code.ecommerce.SpringBootEcommerceApplicationTests;
-
 public class AppTesting extends SpringBootEcommerceApplicationTests {
 	
 	   @Test
